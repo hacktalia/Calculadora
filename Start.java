5e9262612ed7b536a626781c66d6ce72d6a260e5
hacktalia <114227230+hacktalia@users.noreply.github.com>
2022-09-26 22:57:41 +0200

    Add files via upload
    Primera subida

---
+++ Start.java
@@ -0,0 +1,102 @@
+
+ 
+
+import java.util.Scanner;
+
+
+ 
+public class Start {
+	 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Calculadora de Calculator S.A.");
+		
+		Scanner entradaDatos=new Scanner(System.in);
+		
+		float numUno= 0.0f;
+		float numDos= 0.0f;
+		float numTres= 0.0f;
+		float resultado= 0.0f;
+		float opcion=0.0f;
+		float n=0.0f;
+		float nDos=0.0f;
+		
+		
+		boolean operacion =true;
+		
+		System.out.print("Calculadora de Calculator S.A.");
+		
+		System.out.print("Introduzca una operación del menú: ");
+		System.out.print("1.- Suma");
+		System.out.print("2.- Resta");
+		System.out.print("3.- Multiplicar");
+		System.out.print("4.- Dividir");
+		System.out.print("5.- Elija un número mayor de tres números");
+		System.out.print("6.- Número capicua");
+		System.out.print("0.- Salir");
+		
+		
+		System.out.print("1.- suma");
+		System.out.print("introduzca valor número uno");
+		numUno =entradaDatos.nextFloat();
+		System.out.print("introduzca valor número dos");
+		numDos=entradaDatos.nextFloat();
+		resultado = (numUno)+(numDos);
+		return; 
+		
+		System.out.print("2.- resta");
+		System.out.print("introduzca valor número uno");
+		numUno =entradaDatos.nextFloat();
+		System.out.print("introduzca valor número dos");
+		numDos=entradaDatos.nextFloat();
+		resultado = (numUno)-(numDos);
+		return;
+		
+		
+		System.out.print("3.- Multiplicar");
+		System.out.print("introduzca valor número uno");
+		numUno =entradaDatos.nextFloat();
+		System.out.print("introduzca valor número dos");
+		numDos=entradaDatos.nextFloat();
+		resultado = (numUno)*(numDos);
+		return;
+		
+		System.out.print("4.- Dividir");
+		System.out.print("introduzca valor número uno");
+		numUno =entradaDatos.nextFloat();
+		System.out.print("introduzca valor número dos");
+		numDos=entradaDatos.nextFloat();
+		resultado = (numUno)/(numDos);
+		return;
+		
+		
+		System.out.print("5.- Elija un número mayor de tres números  ");
+		System.out.print("introduzca valor número uno");
+		numUno =entradaDatos.nextFloat();
+		System.out.print("introduzca valor número dos");
+		numDos=entradaDatos.nextFloat();
+		System.out.print("introduzca valor número tres");
+		numTres=entradaDatos.nextFloat();
+		if (numUno >0 && numDos >0 && numTres >0);
+		return;
+		
+		
+		System.out.print("6.- capicua");
+	    while (n < 10);
+		resultado= (n) + (inverso);
+		boolean invertido = true;
+		
+		n mayor que 10+ninverso= capicua
+		
+		
+		
+	
+		
+		
+		
+		
+	}
+
+}
