76dc6e507ba40c3cdd506db1ede71b9da40bf8cc
hacktalia <114227230+hacktalia@users.noreply.github.com>
2022-09-29 20:47:19 +0200

    segunda subida
    no logro la opción 6 pero no me da ningún error de código...

--- Start.java
+++ Start.java
@@ -2,7 +2,9 @@
 
 import java.util.Scanner;
 
+
 public class Start {
+
 
 	Scanner sc = null;
 
@@ -11,22 +13,36 @@ public class Start {
 		this.sc = new Scanner(System.in);
 	}
 
-	 
+
 
 	public static void main(String[] args) {
 		
-		Start miCalculadora = new Start();
+		 
+		Start calculadoraTextual = new Start();
 
 		while(true){
-			int opcion = miCalculadora.menu();
+			int opcion = (int) calculadoraTextual.menu();
 
 			switch(opcion){
 				case 1:
-					miCalculadora.opSuma();
+					calculadoraTextual.opSuma();
 					break;
 				case 2:
-					miCalculadora.opResta();
+					calculadoraTextual.opResta();
 					break;
+				case 3:
+					calculadoraTextual.opmultiplicar();
+				break;
+				case 4:
+					calculadoraTextual.opDividir();
+					break;
+				case 5:
+					 calculadoraTextual.opMayorDeTres();
+					break;
+				case 6:
+					calculadoraTextual.opcapicua(opcion);
+				break;
+					
 				case 0:
 					System.out.println("Bye ;)");
 					System.exit(1);
@@ -37,7 +53,7 @@ public class Start {
 		
 	}
 
-	public int menu() {
+	public float menu() {
 		
 		System.out.println("Calculadora de Calculator S.A.");
 		System.out.println("________________________________________\n");
@@ -48,7 +64,7 @@ public class Start {
 		System.out.println("3.- Multiplicar");
 		System.out.println("4.- Dividir");
 		System.out.println("5.- Elija un número mayor de tres números");
-		System.out.println("6.- Número capicua");
+		System.out.println("6.- Capicua");
 		System.out.println("0.- Salir");
 
 		return sc.nextInt();
@@ -62,9 +78,9 @@ public class Start {
 
 		System.out.println("1.- suma");
 		System.out.println("introduzca valor número uno");
-		int x = sc.nextInt();
+		float x=0.0f;
 		System.out.println("introduzca valor número dos");
-		int y = sc.nextInt();
+		float y =0.0f;
 
 		System.out.println("Resultado: " + suma(x,y));
 	}
@@ -72,7 +88,7 @@ public class Start {
 
 
 
-	public int suma(int x, int y) {
+	public float suma(float x, float y) {
 		return x + y;
 	}
 
@@ -85,14 +101,128 @@ public class Start {
 
 		System.out.println("2.- resta");
 		System.out.println("introduzca valor número uno");
-		int x = sc.nextInt();
+		float x = sc.nextFloat();
 		System.out.println("introduzca valor número dos");
-		int y = sc.nextInt();
+		float y = sc.nextFloat();
 
 		System.out.println("Resultado: " + resta(x,y));
 	}
 
-	public int resta(int x, int y) {
+	public float resta(float x, float y) {
 		return x -y;
 	}
-}
+	
+	
+	// ------- Operación de Multiplicar -------	
+
+	public void opmultiplicar() {
+
+		System.out.println("3.- multiplicar");
+		System.out.println("introduzca valor número uno");
+		float x = sc.nextInt();
+		System.out.println("introduzca valor número dos");
+		float y = sc.nextInt();
+
+		System.out.println("Resultado: " + multiplicar(x,y));
+	}
+
+	public float multiplicar(float x, float y) {
+		return x * y;
+	}
+	
+	// ------- Operación de Dividir -------	
+
+		public void opDividir() {
+
+			System.out.println("4.- dividir");
+			System.out.println("introduzca valor número uno");
+			float x = sc.nextFloat();
+			System.out.println("introduzca valor número dos");
+			float y = sc.nextFloat();
+			System.out.println("Resultado: " + Dividir(x,y));
+		}
+
+		public float Dividir(float x, float y) {
+			return x / y;
+		}
+		
+		
+
+		// ------- Número mayor de tres números-------	
+
+		public void opMayorDeTres() {
+
+			System.out.println("5.- Elija un número mayor de tres números");
+			System.out.println("introduzca valor número uno");
+			int x = sc.nextInt();
+			System.out.println("introduzca valor número dos");
+			int y = sc.nextInt();
+			System.out.println ("introduzca valor númeo tres");
+			int z = sc.nextInt();
+			
+
+
+			if (x > y) {
+	            if (x > z) {
+	                System.out.println("El mayor es: " + x);                                             
+	            } else {
+	                System.out.println("el mayor es: " + z);     
+	            }
+	        } else if (y > z) {
+	        	
+	        
+	            System.out.println("el mayor es: " + y);
+	        } else {
+	        	 System.out.println("el mayor es: " + z);
+	        }
+	          
+	        
+		}
+				
+			
+			 // ------- Capicua-------	
+
+			public void opcapicua(int invertido) {
+
+			      System.out.println("esta es la opción 6");
+			      System.out.println ("introduzca un número  : ");
+			      int numero = sc.nextInt();
+			      int resto =sc.nextInt ();
+			      int numInver =sc.nextInt ();
+			     
+					   while (numero!=0)
+					   {
+						   resto=numero%10;
+						   invertido = invertido * 10+ resto;
+						   numero= numero/10;
+					   
+						   
+		if (numero == numInver){
+		
+		
+		System.out.println("Es capicua");
+		
+	          
+		}	else {
+	          System.out.println("No es capicua");
+	          
+		}
+
+	
+		
+		System.out.println ("Fin de las operaciones");
+		
+					   
+					   }
+					   
+
+			
+		
+		}
+}	
+					
+				
+
+
+			
+	
